e24fe18e3f7befb54a5ce4111af659f056db1b6d
mumtazmaqsood <mumtaz.maqsood@yahoo.com>
2022-10-06 07:56:09 +0200

    test code

--- Exception_handling/try_except.py
+++ Exception_handling/try_except.py
@@ -7,5 +7,17 @@ try:
 except ZeroDivisionError:
     print("YOu cannot divide by zero")
 else:
-    print(d)
+    print(resutl)
+
+
+
+no1 = int(input("Enter no1:"))
+no2 = int(input("Enter no2:"))
+
+try:
+    resutl = no1 /no2
+except ZeroDivisionError:
+    print("YOu cannot divide by zero")
+else:
+    print(resutl)
 
