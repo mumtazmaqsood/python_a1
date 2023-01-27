c7dce6f8b3de3f1cfcf2adde4dea6e230b2efe37
mumtazmaqsood <mumtaz.maqsood@yahoo.com>
2023-01-27 14:21:51 +0100

    refactor get_name_function, handling middle name & testcase: def test_first_middle_last_name(self): added

--- Testing/test_name_function.py
+++ Testing/test_name_function.py
@@ -9,6 +9,10 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = get_formatted_name('Mumtaz', 'Maqsood')
         self.assertEqual(formatted_name, 'Mumtaz Maqsood')
 
+    def test_first_middle_last_name(self):
+        #verifying with middle name
+        formatted_name = get_formatted_name('Gul', 'maqsood', "andam")
+        self.assertEqual(formatted_name, 'Gul Andam Maqsood')
 # #what is the behaviour if user given numbers values
 #     def test_given_number_value(self):
 #         self.assertEqual(get_formatted_name(123,456), '123 456')
